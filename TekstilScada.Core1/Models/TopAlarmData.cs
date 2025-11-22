3e6e348cd51a1000f8e5004d0fdb9e44c1042606
Çağatay ERGÜN <cagatay.ergunkou2@outlook.com>
2025-11-22 16:30:35 +0300

    v0.0.32

--- TekstilScada.Core1/Models/TopAlarmData.cs
+++
@@ -1,9 +0,0 @@
-﻿// TekstilScada.Core/Models/TopAlarmData.cs
-namespace TekstilScada.Models
-{
-    public class TopAlarmData
-    {
-        public string AlarmText { get; set; }
-        public int Count { get; set; }
-    }
-}
